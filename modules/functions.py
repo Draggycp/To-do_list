def39bc2228b195b83166f8b5a7e9df7e73b1ca0
Draguya <alexandre.c.pansera@gmail.com>
2023-07-18 15:22:22 -0400

    Implement the add button

--- modules/functions.py
+++ modules/functions.py
@@ -1,10 +1,12 @@
+FILEPATH = 'files/todos.txt'
+
 def get_todos(filepath):
     with open(filepath, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
